49314e2d2faed40c3d9cdeeced40d928a9f5cf73
aliali <142698095+alisinga@users.noreply.github.com>
2024-02-08 16:54:02 +0400

    Add files via upload

---
+++ ft_printf/ft_printf.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aliali <aliali@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/05 19:23:14 by aliali            #+#    #+#             */
+/*   Updated: 2024/02/05 20:15:07 by aliali           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF
+# define FT_PRINTF
+
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <limits.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <stddef.h>
+# include <stdarg.h>
+
+#endif
